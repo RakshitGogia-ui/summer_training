b6ee9adce1ee47eb2f288f556cdc36aa2e334775
RakshitGogia-ui <gogiarakshit00@gmail.com>
2026-07-13 07:24:03 +0530

    adidas

--- day6/my-react-app/src/hooks/useCart.js
+++ day6/my-react-app/src/hooks/useCart.js
@@ -1,8 +1,6 @@
 import { useContext } from "react";
 import { CartContext } from "../Context/CartContext";
 
-console.log("CartContext =", CartContext);
-
 export default function useCart() {
   return useContext(CartContext);
 }
